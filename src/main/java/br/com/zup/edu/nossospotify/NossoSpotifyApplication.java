63c45448867d325e2e63c207629ffa2966bd32c6
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-13 19:04:27 -0300

    Format files and update configs and gitignore

--- src/main/java/br/com/zup/edu/nossospotify/NossoSpotifyApplication.java
+++ src/main/java/br/com/zup/edu/nossospotify/NossoSpotifyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NossoSpotifyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NossoSpotifyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NossoSpotifyApplication.class, args);
+    }
 
 }
